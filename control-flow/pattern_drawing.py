1f0a7a68f6a525df06e6c72757fe43225b9c8350
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-05-28 15:58:34 +0100

    Add script to draw a square pattern of asterisks of user-defined size

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+# A square pattern of asterisks (`*`) of user-defined sizes
+
+pattern_size = int(input("Enter the size of the pattern: "))
+
+active = True
+while active:
+
+    for i in range(pattern_size):
+        result = "*" *  pattern_size
+        print(result, end="")
+        print()
+    active = False
