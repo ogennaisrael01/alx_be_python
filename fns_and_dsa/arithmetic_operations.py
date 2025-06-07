470df5f0c5e6a68feb06856b3838c4fd51228e20
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-06-07 08:36:32 +0100

    add operations to the arithmetic and main module

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,13 +4,13 @@ def perform_operation(num1, num2, operation):
     """
     
     match operation: 
-        case "+":
+        case "add":
             return  num1 + num2 
-        case "-":
+        case "subtract":
             return num1 - num2
-        case "*":
+        case "multiply":
             return  num1 * num2
-        case "/":
+        case "divide":
             if num2 == 0:
                 print("Error: Can't divide by zero")
                 return None 
--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,11 +1,11 @@
-from arithmethic_operations import perform_operation # imported function from arithmetic_operations module
+from arithmetic_operations import perform_operation # imported function from arethmetic_operations module
 
 def main():
-    """ calculator main function """
+    """Main function to run the arithmetic operations calculator."""
     print("Arithmetic Operations")
     num1 = float(input("Enter the first number: ")) # input first number
     num2 = float(input("Enter the second number: ")) # input second number
-    operation = input("Enter the operation ('+', '-', '*', '/'): ") # input operation to do calculation
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").lower() # input operation to do calculation
 
     result = perform_operation(num1, num2, operation)
     print(f"Result: {result}")
