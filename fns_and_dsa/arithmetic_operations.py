5ef9ac416c751bbc5b5c079cc998088a5557bb8e
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-06-07 08:42:32 +0100

    change from match case to if-elif-else chain

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,18 +2,18 @@ def perform_operation(num1, num2, operation):
     """
     A simple calculator function
     """
-    
-    match operation: 
-        case "add":
-            return  num1 + num2 
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return  num1 * num2
-        case "divide":
-            if num2 == 0:
-                print("Error: Can't divide by zero")
-                return None 
-            return num1 / num2
-        case _:
-            print("Don't have a calculation on this operator") 
+
+    if operation == "add":
+        return num1 + num2 
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            print("Error: Can't divide by zero")
+            return None
+        return num1 / num2
+    else:
+         print("Don't have a calculation on this operator")
+           
