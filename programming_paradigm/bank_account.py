ff48a6417883025454598354b2cb91c319d54f1f
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-06-11 20:10:50 +0100

    oop programming

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -25,4 +25,4 @@ class BankAccount:
         return True
        
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}.")
+        print(f"Current Balance: {self.account_balance}.")
