c54347518463faa041280b26e5b85e626254e837
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-06-19 16:17:57 +0100

    dunder method in a book class

---
+++ oop/book_class.py
@@ -0,0 +1,17 @@
+class Book:
+    # constructor method
+    def __init__(self, title, author, year):
+        self.author  = author
+        self.title = title
+        self.year = year
+
+    # Represent an object as a sting
+    def __str__(self):
+        return f"{self.title} by {self.author}, Published in {self.year}"
+     
+    #String representation of the object
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    # destuctor method
+    def __del__(self):
+        print(f"Deleting {self.title}")
